7d9f2ad29856ab856ee249ba78a15e05fe345660
sfonch <sfonchik@gmail.com>
2025-12-18 21:59:06 +0200

    Added root folder (2x)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-// use wallpaper;
+use wallpaper;
 use rfd;
+// use serde_json;
+use dirs;
+use std::fs;
 
 fn main() {
+    create_folder();
     set_wallpaper();
 }
+
+fn create_folder() {
+    let mut path = dirs::cache_dir().unwrap();
+    path.push("Lumadesk");
+    path.push("wallpapers");
+
+    fs::create_dir_all(&path).unwrap();
+}
+
 
 fn set_wallpaper() {
     let path_buf = rfd::FileDialog::new()
