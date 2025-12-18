46072b783a9f93664617d34fe8d3953e18f55d7e
sfonch <sfonchik@gmail.com>
2025-12-18 14:15:24 +0200

    Main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// use wallpaper;
+use rfd;
+
 fn main() {
-    println!("Hello, world!");
+    set_wallpaper();
+}
+
+fn set_wallpaper() {
+    let path_buf = rfd::FileDialog::new()
+        .add_filter("image", &["png", "jpeg"])
+        .pick_file()
+        .unwrap();
+
+    let path_str = path_buf.to_string_lossy();
+
+    wallpaper::set_from_path(&path_str).unwrap();
 }
